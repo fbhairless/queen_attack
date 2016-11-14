214354416d2432ec0950a3e7acb3cb892521de5e
Hal Fairless and Jonathan Thom <jonathan@>
2016-11-14 15:29:47 -0800

    Will return true for diagonal queen attacks

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -2,6 +2,10 @@ class Array
   define_method(:queen_attack) do |coordinates|
     if self[0].eql?(coordinates[0])
       true
+    elsif (self[0].-(coordinates[0])).abs.eql?((self[1].-(coordinates[1])).abs)
+      true
+    elsif self[1].eql?(coordinates[1])
+      true
     else
       false
     end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -9,4 +9,12 @@ describe('Array#queen_attack') do
   it('Queen can attack vertically when the first coordinate of each pair is equal') do
     expect([1,1].queen_attack([1,7])).to(eq(true))
   end
+
+  it('Queen can attack horizontally when the second coordinate of each pair is equal') do
+    expect([1,1].queen_attack([7,1])).to(eq(true))
+  end
+
+  it('Queen can attach diagonally when the absolute value difference of each pairs first coordinate is equal to the absolute value difference of each pairs second coordinate') do
+    expect([5,3].queen_attack([3,1])).to(eq(true))
+  end
 end
